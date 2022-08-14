3972112ccd0e2463cc3e85018a0b6eb35715158d
unknown <victornerikokado@yahoo.com>
2022-08-14 15:26:49 -0300

    fix

--- delivery.py
+++ delivery.py
@@ -23,7 +23,3 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-if __name__ == "__main__":
-    app.run()
